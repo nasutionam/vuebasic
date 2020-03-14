fb58348f68a93cc0d9f0694ba885b1246490a9ce
Muhammad Asri Nasution <nasution.nam@gmail.com>
2020-03-14 21:24:16 +0700

    list rendering

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ var app = new Vue({
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "Mantab",
         inStock: true,
-        inventory: 9
+        inventory: 9,
+        details: ["80% cotton", "20% polyester", "Gender-neutral"],
+        variants: [
+            {
+                varianId: 2234,
+                variantColor: "green"
+            },
+            {
+                variantId: 2235,
+                variantColor: "blue"
+            }
+        ]
     }
 })
